00bec43a77d9a6563b96c7dd32ba588e8615478f
Alonso Avila <avila@zaa.mx>
2025-07-08 18:47:15 -0600

    cambio copy

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -34,7 +34,7 @@ const Hero = () => {
               We Now, el momento es ahora
             </h1>
             <p className="mt-8 text-lg font-medium text-pretty text-white sm:text-xl/8">
-              Únete a WE NOW y transforma tu vida con productos de bienestar de alta calidad y una oportunidad de negocio que te permite ganar mientras ayudas a otros a mejorar su salud.
+              Únete a WE NOW y transforma tu vida con productos de bienestar de alta calidad.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a href="/account" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
