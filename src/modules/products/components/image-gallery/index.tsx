58cae8706f9e2ce42dd9335d11eef49b80c821f6
Evbera <soporte@evbera.com>
2025-08-28 01:46:26 -0600

    Cambio en la galeria de imagen del producto

--- src/modules/products/components/image-gallery/index.tsx
+++ src/modules/products/components/image-gallery/index.tsx
@@ -14,7 +14,8 @@ const ImageGallery = ({ images }: ImageGalleryProps) => {
           return (
             <Container
               key={image.id}
-              className="relative aspect-[29/34] w-full overflow-hidden bg-ui-bg-subtle"
+              // Change this class to make the container square
+              className="relative aspect-square w-full overflow-hidden bg-ui-bg-subtle"
               id={image.id}
             >
               {!!image.url && (
