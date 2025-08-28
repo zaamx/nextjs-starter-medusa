0f0d63572631718bbe1a09439c887ac3a8eda566
Evbera <soporte@evbera.com>
2025-08-27 23:48:55 -0600

    menu desplegable para ordenar productos en tienda

--- src/modules/store/components/refinement-list/sort-products/index.tsx
+++ src/modules/store/components/refinement-list/sort-products/index.tsx
@@ -1,47 +1,59 @@
 "use client"
+import { useState } from "react"
 
-import FilterRadioGroup from "@modules/common/components/filter-radio-group"
-
-export type SortOptions = "price_asc" | "price_desc" | "created_at"
+export type SortOptions = "price_asc" | "price_desc" | "created_at" | "default"
 
 type SortProductsProps = {
-  sortBy: SortOptions
-  setQueryParams: (name: string, value: SortOptions) => void
+  sortBy: SortOptions | undefined
+  setQueryParams: (name: string, value: SortOptions | undefined) => void
   "data-testid"?: string
 }
 
 const sortOptions = [
-  {
-    value: "created_at",
-    label: "Últimos Arribos",
-  },
-  {
-    value: "price_asc",
-    label: "Precio: Menor -> Mayor",
-  },
-  {
-    value: "price_desc",
-    label: "Precio: Mayor -> Menor",
-  },
+  { value: "default", label: "Ordenar por" },
+  { value: "created_at", label: "Últimos Arribos" },
+  { value: "price_asc", label: "Precio: Menor -> Mayor" },
+  { value: "price_desc", label: "Precio: Mayor -> Menor" },
 ]
 
-const SortProducts = ({
-  "data-testid": dataTestId,
-  sortBy,
-  setQueryParams,
-}: SortProductsProps) => {
-  const handleChange = (value: SortOptions) => {
+const SortProducts = ({ "data-testid": dataTestId, setQueryParams }: SortProductsProps) => {
+  const [activeSortBy, setActiveSortBy] = useState<SortOptions>("default")
+  const [disableDefault, setDisableDefault] = useState(false)
+
+  const handleChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value as SortOptions
+
+    // Si se selecciona "default" NO hacemos nada
+    if (value === "default") {
+      setActiveSortBy("default")
+      return
+    }
+
+    // Una vez que selecciona otra opción, bloqueamos la default
+    setDisableDefault(true)
+    setActiveSortBy(value)
     setQueryParams("sortBy", value)
   }
 
   return (
-    <FilterRadioGroup
-      title="Ordenar por"
-      items={sortOptions}
-      value={sortBy}
-      handleChange={handleChange}
-      data-testid={dataTestId}
-    />
+    <div className="flex gap-2 items-center" data-testid={dataTestId}>
+      <select
+        id="sort-by"
+        onChange={handleChange}
+        value={activeSortBy}
+        className="text-sm p-2 border rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+      >
+        {sortOptions.map((option) => (
+          <option
+            key={option.value}
+            value={option.value}
+            disabled={option.value === "default" && disableDefault}
+          >
+            {option.label}
+          </option>
+        ))}
+      </select>
+    </div>
   )
 }
 
