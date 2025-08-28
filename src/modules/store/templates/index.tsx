5b0e81919a0c98baa20530699e8a56843904a31b
Evbera <soporte@evbera.com>
2025-08-28 00:32:22 -0600

    menu desplegable parte superior derecha

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -6,6 +6,8 @@ import { SortOptions } from "@modules/store/components/refinement-list/sort-prod
 
 import PaginatedProducts from "./paginated-products"
 
+const DEFAULT_SORT: SortOptions = "default" 
+
 const StoreTemplate = ({
   sortBy,
   page,
@@ -16,26 +18,22 @@ const StoreTemplate = ({
   countryCode: string
 }) => {
   const pageNumber = page ? parseInt(page) : 1
-  const sort = sortBy || "created_at"
+  const sort = sortBy ?? DEFAULT_SORT
 
   return (
-    <div
-      className="flex flex-col small:flex-row small:items-start py-6 content-container"
-      data-testid="category-container"
-    >
-      <RefinementList sortBy={sort} />
-      <div className="w-full">
-        <div className="mb-8 text-2xl-semi">
-          <h1 data-testid="store-page-title">Todos los productos</h1>
-        </div>
-        <Suspense fallback={<SkeletonProductGrid />}>
-          <PaginatedProducts
-            sortBy={sort}
-            page={pageNumber}
-            countryCode={countryCode}
-          />
-        </Suspense>
+    <div className="flex flex-col py-6 content-container" data-testid="category-container">
+      
+      <div className="flex justify-between items-center mb-6 w-full">
+        <h1 className="text-2xl-semi" data-testid="store-page-title">
+          Todos los productos
+        </h1>
+
+        <RefinementList sortBy={sort} />
       </div>
+
+      <Suspense fallback={<SkeletonProductGrid />}>
+        <PaginatedProducts sortBy={sort} page={pageNumber} countryCode={countryCode} />
+      </Suspense>
     </div>
   )
 }
