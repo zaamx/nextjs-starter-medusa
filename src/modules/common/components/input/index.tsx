9384306ce5e47c4b8fcb0052071c0a8816d53d39
Evbera <soporte@evbera.com>
2026-05-13 20:43:39 -0600

    correccion de texto en inicio de sesión

--- src/modules/common/components/input/index.tsx
+++ src/modules/common/components/input/index.tsx
@@ -44,17 +44,17 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
             name={name}
             placeholder=" "
             required={required}
-            className="pt-4 pb-1 block w-full h-11 px-4 mt-0 bg-ui-bg-field border rounded-md appearance-none focus:outline-none focus:ring-0 focus:shadow-borders-interactive-with-active border-ui-border-base hover:bg-ui-bg-field-hover"
+            className="floating-input pt-5 pb-1 block w-full h-11 px-4 mt-0 bg-ui-bg-field border rounded-md appearance-none focus:outline-none focus:ring-0 focus:shadow-borders-interactive-with-active border-ui-border-base hover:bg-ui-bg-field-hover transition-colors duration-150"
             {...props}
             ref={inputRef}
           />
           <label
             htmlFor={name}
             onClick={() => inputRef.current?.focus()}
-            className="flex items-center justify-center mx-3 px-1 transition-all absolute duration-300 top-3 -z-1 origin-0 text-ui-fg-subtle"
+            className="floating-label absolute top-3 left-4 origin-top-left text-ui-fg-subtle transition-all duration-300 cursor-text"
           >
             {label}
-            {required && <span className="text-rose-500">*</span>}
+            {required && <span className="text-rose-500 ml-1">*</span>}
           </label>
           {type === "password" && (
             <button
